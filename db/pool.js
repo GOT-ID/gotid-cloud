c9861a8981d153e761c71043029309e8e1c936da
GOT-ID <got-id@outlook.com>
2025-12-09 13:51:46 +0000

    Create pool.js

---
+++ db/pool.js
@@ -0,0 +1,12 @@
+// db/pool.js
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }  // Render Postgres
+    : false,
+});
+
+export default pool;
