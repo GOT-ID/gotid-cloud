1a28c8361e750d05e9424043e3bc8f945f2772b4
GOT-ID <got-id@outlook.com>
2026-04-18 13:38:06 +0100

    Update and rename sql/sql/002_scans_table.sql to sql/002_scans_table.sql

--- sql/002_scans_table.sql
+++ sql/002_scans_table.sql
@@ -1,7 +1,7 @@
 -- 002_scans_table.sql
 -- Create table to store GOT-ID scanner events
 
-CREATE TABLE IF NOT EXISTS scans (
+CREATE TABLE IF NOT EXISTS scan_events (
   id SERIAL PRIMARY KEY,
   uuid TEXT NOT NULL,
   counter INTEGER,
