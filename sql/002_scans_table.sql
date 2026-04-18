10042bc1b6db87192f3cec99803b9b74b1140eef
GOT-ID <got-id@outlook.com>
2026-04-18 14:04:36 +0100

    Update 002_scans_table.sql

--- sql/002_scans_table.sql
+++ sql/002_scans_table.sql
@@ -1,7 +1,7 @@
 -- 002_scans_table.sql
 -- Create table to store GOT-ID scanner events
 
-CREATE TABLE IF NOT EXISTS scan_events (
+CREATE TABLE IF NOT EXISTS scans (
   id SERIAL PRIMARY KEY,
   uuid TEXT NOT NULL,
   counter INTEGER,
